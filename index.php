a8f9163543f528bd33ffcaaec29becb6ca6877da
akuyaesekon <akuyaesekon@gmail.com>
2023-07-20 10:15:01 +0300

    changes hapa na pale

--- index.php
+++ index.php
@@ -5,8 +5,8 @@ require 'init.php';
 <body background="image/bg2.jpg">
    <div class="container start-container">
      <div class="col-md-6 title-text">
-         <img src="image/polylogo.png" class="img-rounded img-responsive" width="100" height="100">
-     	<h4>The Polytechnic, Ibadan</h4>
+         <img src="image/zetech.png" class="img-rounded img-responsive" width="100" height="100">
+     	<h4>Zetech University</h4>
      	<h1>STUDENTS' PROJECT ALLOCATION & MANAGEMENT SYSTEM</h1>
      </div>
      <div class="col-md-4">
